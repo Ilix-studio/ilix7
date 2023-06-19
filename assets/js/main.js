4e4053078b531e7550d201c6b6046c5937790719
ilix-studio <ilishjyoti17@gmail.com>
2023-06-20 01:20:32 +0530

    header added

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/* SHOW MENU */
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/* REMOVE MENU MOBILE */
+
+/* ADD BLUR TO HEADER */
+
+/* SHOW SCROLL UP */
+
+/* SCROLL SECTIONS ACTIVE LINK */
+
+/* SCROLL REVEAL ANIMATION */
