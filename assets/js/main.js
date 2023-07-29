1355f16e00c830a18be10f8ea97fbcb4cd405d45
ilix-studio <ilishjyoti17@gmail.com>
2023-07-29 23:53:42 +0530

    header||nav/main/about/popular/explore/join/footer\withCSSandJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,34 @@ const blurHeader = () => {
 };
 window.addEventListener("scroll", blurHeader);
 /* SHOW SCROLL UP */
+const scrollUP = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  //When the scrollup is higher than 350 viewPoet height, and the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUP.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUP);
 
 /* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll("section[id]");
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+    const sectionsClass = document.querySelector(
+      ".nav__menu a[href*=" + sectionId + "]"
+    );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 
 /* SCROLL REVEAL ANIMATION */
